10a41cd24a7c763834a9f66e6f429f0df4c4eda8
Jeraphan Boonsin <jeraphan2000@gmail.com>
2021-10-20 03:05:12 +0700

    add banner from network

--- lib/home.dart
+++ lib/home.dart
@@ -4,6 +4,7 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'constant.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -13,25 +14,54 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final List<String> imageList = [
+    "https://images.unsplash.com/photo-1593642702909-dec73df255d7?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1469&q=80",
+    "https://images.unsplash.com/photo-1593642634402-b0eb5e2eebc9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60"
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor,
-      body: Padding(
-        padding: const EdgeInsets.only(top: 70.0, left: 30.0),
-        child: Row(
-          children: [
-            Text(
-              "Food Menu",
-              style: TextStyle(
-                  fontSize: 28, 
-                  fontWeight: 
-                  FontWeight.w800, 
-                  color: darkColor),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+                top: 70.0, left: 20.0, right: 20.0, bottom: 20.0),
+            child: Row(
+              children: [
+                Text(
+                  "Food Menu",
+                  style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.w800,
+                      color: darkColor),
+                ),
+              ],
+            ),
+          ),
+        CarouselSlider(
+          items: imageList.map((e) => ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: Stack(
+              fit: StackFit.expand,
+              children: <Widget>[
+                Image.network(e,
+                fit: BoxFit.cover,)
+              ],
             )
-          ],
-        ),
+          )).toList(), 
+          options: CarouselOptions(
+            enlargeCenterPage: true,
+            enableInfiniteScroll: false,
+            autoPlay: true))
+          // ClipRRect(
+          //   borderRadius: BorderRadius.circular(26.0),
+          //   child: Image.asset("assets/images/banner.jpg",),
+          // )
+        ],
       ),
+      
     );
   }
 }
